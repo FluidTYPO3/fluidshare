399dac59b783860c00f89d8a737cd6473dd00ca0
Claus Due <claus@namelesscoder.net>
2014-08-24 03:32:18 +0200

    [TASK] Finish controller class

--- Classes/Controller/GistController.php
+++ Classes/Controller/GistController.php
@@ -10,6 +10,7 @@ use FluidTYPO3\Fluidshare\Fetcher\Response;
 use FluidTYPO3\Vhs\Asset;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 /**
  * Class GistController
@@ -58,11 +59,33 @@ class GistController extends ActionController {
 	}
 
 	/**
+	 * @param integer $tag
+	 * @param integer $extension
 	 * @return void
 	 */
-	public function listAction() {
-		$gists = $this->gistRepository->findAll();
+	public function listAction($tag = NULL, $extension = NULL) {
+		$query = $this->gistRepository->createQuery();
+		$constraints = array();
+		if (NULL !== $tag) {
+			$constraints[] = $query->contains('tags', $tag);
+		}
+		if (NULL !== $extension) {
+			$constraints[] = $query->contains('extensions', $extension);
+		}
+		if (1 === count($constraints)) {
+			$query->matching($constraints[0]);
+		} elseif (0 < count($constraints)) {
+			$query->matching($query->logicalAnd($constraints));
+		}
+		$gists = $query->execute();
 		$this->view->assign('gists', $gists);
+		$this->view->assign('tags', $this->tagRepository->findAll());
+		$this->view->assign('extensions', $this->extensionRepository->findAll());
+		// emulated previous request parameters for preselection in form
+		$this->view->assign('submission', array(
+			'tags' => array($tag),
+			'extensions' => array($extension)
+		));
 	}
 
 	/**
@@ -73,6 +96,8 @@ class GistController extends ActionController {
 		$response = $this->fetchGistDataAndLoadStylesheet($gist);
 		$this->view->assign('gist', $gist);
 		$this->view->assign('response', $response);
+		$this->view->assign('tags', $this->tagRepository->findAll());
+		$this->view->assign('extensions', $this->extensionRepository->findAll());
 	}
 
 	/**
@@ -80,12 +105,12 @@ class GistController extends ActionController {
 	 * @return void
 	 */
 	public function newAction(Gist $gist = NULL) {
-		if (NULL === $gist) {
-			$gist = $this->objectManager->get('FluidTYPO3\Fluidshare\Domain\Model\Gist');
-		}
 		$this->view->assign('gist', $gist);
 		$this->view->assign('tags', $this->tagRepository->findAll());
 		$this->view->assign('extensions', $this->extensionRepository->findAll());
+		if (NULL !== $this->request->getOriginalRequest()) {
+			$this->view->assign('submission', $this->request->getOriginalRequest()->getArgument('gist'));
+		}
 	}
 
 	/**
@@ -103,6 +128,7 @@ class GistController extends ActionController {
 	 * @return void
 	 */
 	public function createAction(Gist $gist) {
+		$this->addFlashMessage(LocalizationUtility::translate('messages.created', 'fluidshare'));
 		$this->gistRepository->add($gist);
 		$this->redirect('list');
 	}
@@ -117,6 +143,13 @@ class GistController extends ActionController {
 		$stylesheet = ObjectAccess::getPropertyPath($response, 'json.stylesheet');
 		Asset::createFromUrl($stylesheet);
 		return $response;
+	}
+
+	/**
+	 * @return null
+	 */
+	protected function getErrorFlashMessage() {
+		return FALSE;
 	}
 
 }
