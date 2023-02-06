ab8d17c76e7d146b188c3fb032e2240e44634463
Claus Due <claus@namelesscoder.net>
2023-02-06 23:10:16 +0100

    [TASK] Update plugin registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,11 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Fluidshare', 'Display',
-	array('Gist' => 'list,display'),
-	array('Gist' => 'list')
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('Fluidshare', 'Display',
+	[\FluidTYPO3\Fluidshare\Controller\GistController::class => 'list,display'],
+	[\FluidTYPO3\Fluidshare\Controller\GistController::class => 'list']
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Fluidshare', 'Submit',
-	array('Gist' => 'new,confirm,create'),
-	array('Gist' => 'confirm,create')
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('Fluidshare', 'Submit',
+	[\FluidTYPO3\Fluidshare\Controller\GistController::class => 'new,confirm,create'],
+	[\FluidTYPO3\Fluidshare\Controller\GistController::class => 'confirm,create']
 );
